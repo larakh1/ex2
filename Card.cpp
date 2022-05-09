1c4ce0b50b14fae08cd3118fa7c071d22b6d1236
larakh1 <103194777+larakh1@users.noreply.github.com>
2022-05-09 22:41:03 +0300

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -41,6 +41,28 @@ void Card::applyEncounter(Player& player) const
         }
     }
 
-
-
 }
+Card::Card(CardType type, const CardStats& stats): m_effect(type), m_stats(stats){}
+
+
+void Card::printInfo() const
+{
+    if (m_effect==CardType::Treasure)
+    {
+        printTreasureCardInfo(m_stats);
+    }
+    if (m_effect==CardType::Buff)
+    {
+        printBuffCardInfo(m_stats);
+    }
+     if(m_effect==CardType::Heal)
+    {
+        printHealCardInfo(m_stats);
+    }
+    if(m_effect==CardType::Battle)
+    {
+        printBattleCardInfo(m_stats);
+    }
+}
+
+
