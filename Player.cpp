3a7b7f487cd3a80684c1a94072841e3d11d5e79e
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-09 20:31:10 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,82 +1,70 @@
-#include "Player.h"
-#include <string>
+#ifndef EX2_Player_H
+#define EX2_Player_H
+
+#include <iostream>
+#include "utilities.h"
 
 
 using std::string;
 
-Player::Player(const char* const name, int maxHP, int force) :
-        m_name(name),m_maxHP(maxHP),m_HP(maxHP),m_force(force),m_level(1),m_coins(0){}
+class Player{
+
+public:
+    /**
+     *
+     * @param name
+     * @param maxHP
+     * @param force
+     */
+
+    Player(const char* const name, int maxHP=MAX_DEFAULT_HP,int force=MAX_DEFAULT_FORCE);
+/**
+ *
+ * @param name
+ * @param maxHP
+ * @param force
+ */
+    Player(const Player& player)=default;
+
+    ~Player()=default;
+
+    Player& operator=(const Player& player)=default;
+
+    void printInfo() const;
+
+    void levelUp();
+
+    int getLevel() const;
+
+    void buff(const int add);
+
+    void heal(const int add);
+
+    void damage(const int decrease);
+
+    bool isKnockedOut() const;
+
+    void addCoins(const int add);
+
+    bool pay(const int price);
+
+    int getAttackStrength() const;
+
+
+private:
+    const char* const m_name;
+    int m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    int m_coins;
+    static const int MAX_LEVEL=10;
+    static const int MAX_DEFAULT_HP=100;
+    static const int MAX_DEFAULT_FORCE=5;
+
+
+};
 
 
 
-void Player::printInfo() const
-{
-    printPlayerInfo(m_name,m_level,m_force,m_HP,m_coins);
-}
-
-
-int Player::getLevel() const {
-    return m_level;
-}
-
-void Player::heal(const int add)
-{
-    if(m_HP!=m_maxHP)
-    {
-        m_HP+=add;
-        if(m_HP>m_maxHP)
-        {
-            m_HP=m_maxHP;
-        }
-    }
-}
-
-bool Player::isKnockedOut() const {
-    return m_HP==0;
-}
-
-bool Player::pay(const int price) {
-    if(m_coins-price<0)
-    {
-        return false;
-    }
-    else
-    {
-        m_coins-=price;
-        return true;
-    }
-}
-
-void Player::levelUp()
-{
-    if(m_level!=10)
-    {
-        m_level++;
-    }
-}
-
-void Player::buff(const int add)
-{
-    m_force+=add;
-}
-
-void Player::damage(const int decrease)
-{
-    m_HP-=decrease;
-    if(m_HP<0)
-    {
-        m_HP=0;
-    }
-}
-
-void Player::addCoins(const int add)
-{
-    m_coins+=add;
-}
-
-int Player::getAttackStrength() const
-{
-    return m_force+m_level;
-}
-
-
+#endif //EX2_Player_H
