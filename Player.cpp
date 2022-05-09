c94702b4627126eee7961aaab1d031383ed94199
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-09 19:58:19 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -7,6 +7,7 @@ using std::string;
 Player::Player(const char* const name, int maxHP, int force) :
    m_name(name),m_maxHP(maxHP),m_HP(maxHP),m_force(force),m_level(1),m_coins(0){}
 
+ 
 
    void Player::printInfo() const
    {
@@ -44,5 +45,38 @@ Player::Player(const char* const name, int maxHP, int force) :
            return true;
        }
    }
+
+void Player::levelUp()
+{
+    if(m_level!=10)
+    {
+        m_level++;
+    }
+}
+
+void Player::buff(const int add)
+{
+    m_force+=add;
+}
+
+void Player::damage(const int decrease)
+{
+    m_HP-=decrease;
+    if(m_HP<0)
+    {
+        m_HP=0;
+    }
+}
+
+void Player::addCoins(const int add)
+{
+    m_coins+=add;
+}
+
+int Player::getAttackStrength() const
+{
+    return m_force+m_level;
+}
+
 
 
