dff634bc184bbd0193bbce8aec46ac17c4430e59
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-09 23:31:26 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,70 +1,112 @@
-#ifndef EX2_Player_H
-#define EX2_Player_H
-
-#include <iostream>
-#include "utilities.h"
+#include "Player.h"
+#include <string>
 
 
 using std::string;
 
-class Player{
-
-public:
-    /**
-     *
-     * @param name
-     * @param maxHP
-     * @param force
-     */
-
-    Player(const char* const name, int maxHP=MAX_DEFAULT_HP,int force=MAX_DEFAULT_FORCE);
-/**
- *
- * @param name
- * @param maxHP
- * @param force
- */
-    Player(const Player& player)=default;
-
-    ~Player()=default;
-
-    Player& operator=(const Player& player)=default;
-
-    void printInfo() const;
-
-    void levelUp();
-
-    int getLevel() const;
-
-    void buff(const int add);
-
-    void heal(const int add);
-
-    void damage(const int decrease);
-
-    bool isKnockedOut() const;
-
-    void addCoins(const int add);
-
-    bool pay(const int price);
-
-    int getAttackStrength() const;
-
-
-private:
-    const char* const m_name;
-    int m_level;
-    int m_force;
-    int m_maxHP;
-    int m_HP;
-    int m_coins;
-    static const int MAX_LEVEL=10;
-    static const int MAX_DEFAULT_HP=100;
-    static const int MAX_DEFAULT_FORCE=5;
-
-
-};
+Player::Player(const char* name, int maxHP, int force) :
+m_name(name),m_maxHP(maxHP),m_HP(maxHP),m_force(force),m_level(1),m_coins(0)
+{   if(maxHP<=0)
+    {
+        m_maxHP = MAX_DEFAULT_HP;
+        m_HP=MAX_DEFAULT_HP;
+    }
+    if(force<=0)
+    {
+        m_force = MAX_DEFAULT_FORCE;
+    }
 
 
 
-#endif //EX2_Player_H
+}
+
+
+
+void Player::printInfo() const
+{
+    printPlayerInfo(m_name,m_level,m_force,m_HP,m_coins);
+}
+
+
+int Player::getLevel() const {
+    return m_level;
+}
+
+void Player::heal(const int add)
+{ if (add<0)
+    {
+    return;
+    }
+    if(m_HP!=m_maxHP)
+    {
+        m_HP+=add;
+        if(m_HP>m_maxHP)
+        {
+            m_HP=m_maxHP;
+        }
+    }
+}
+
+bool Player::isKnockedOut() const {
+    return m_HP==0;
+}
+
+bool Player::pay(const int price) {
+ if (price<=0)
+ {
+     return true;
+ }
+    if(m_coins-price<0)
+    {
+        return false;
+    }
+    else
+    {
+        m_coins-=price;
+        return true;
+    }
+}
+
+void Player::levelUp()
+{
+    if(m_level!=MAX_LEVEL)
+    {
+        m_level++;
+    }
+}
+
+void Player::buff(const int add)
+{
+    if(add>=0)
+    {
+        m_force += add;
+    }
+}
+
+void Player::damage(const int decrease)
+{
+    if(decrease<0)
+    {
+        return;
+    }
+    m_HP-=decrease;
+    if(m_HP<0)
+    {
+        m_HP=0;
+    }
+}
+
+void Player::addCoins(const int add)
+{
+    if(add>=0)
+    {
+        m_coins += add;
+    }
+}
+
+int Player::getAttackStrength() const
+{
+    return m_force+m_level;
+}
+
+
