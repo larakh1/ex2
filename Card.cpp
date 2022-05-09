042086dedc87584481be814d2b74cca4abff1eea
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-09 22:28:23 +0300

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,46 @@
+#include "card.h"
+
+void Card::applyEncounter(Player& player) const
+{
+    if(m_effect==CardType::Buff)
+    {  if(player.pay(m_stats.cost))
+        {
+           player.buff(m_stats.buff);
+        }
+    }
+
+    if(m_effect==CardType::Heal)
+    {  if(player.pay(m_stats.cost))
+        {
+            player.heal(m_stats.heal);
+        }
+    }
+
+    if(m_effect==CardType::Treasure)
+    {
+        player.addCoins(m_stats.loot);
+    }
+
+    if(m_effect==CardType::Battle)
+    {
+        bool isWinner=false;
+        if(player.getAttackStrength()>=m_stats.force)
+        {
+            player.levelUp();
+            player.addCoins(m_stats.loot);
+            isWinner= true;
+            printBattleResult(isWinner);
+        }
+        else
+        {
+            player.damage(m_stats.hpLossOnDefeat);
+            if(player.isKnockedOut())
+            {
+                printBattleResult(isWinner);
+            }
+        }
+    }
+
+
+
+}
