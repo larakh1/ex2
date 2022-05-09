c28c88a35edda611f548b394c31dc93cbcf4517f
ShadaSabea <103149059+ShadaSabea@users.noreply.github.com>
2022-05-10 00:12:04 +0300

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,70 +1,57 @@
 #include "Card.h"
 
-void Card::applyEncounter(Player& player) const
-{
-    if(m_effect==CardType::Buff)
-    {  if(player.pay(m_stats.cost))
-        {
-           player.buff(m_stats.buff);
+void Card::applyEncounter(Player& player) const {
+    if (m_effect == CardType::Buff) {
+        if (player.pay(m_stats.cost)) {
+            player.buff(m_stats.buff);
         }
     }
 
-    if(m_effect==CardType::Heal)
-    {  if(player.pay(m_stats.cost))
-        {
+    if (m_effect == CardType::Heal) {
+        if (player.pay(m_stats.cost)) {
             player.heal(m_stats.heal);
         }
     }
 
-    if(m_effect==CardType::Treasure)
-    {
+    if (m_effect == CardType::Treasure) {
         player.addCoins(m_stats.loot);
     }
 
-    if(m_effect==CardType::Battle)
-    {
-        bool isWinner=false;
-        if(player.getAttackStrength()>=m_stats.force)
-        {
+    if (m_effect == CardType::Battle) {
+        bool isWinner = false;
+        if (player.getAttackStrength() >= m_stats.force) {
             player.levelUp();
             player.addCoins(m_stats.loot);
-            isWinner= true;
+            isWinner = true;
             printBattleResult(isWinner);
-        }
-        else
-        {
+        } else {
             player.damage(m_stats.hpLossOnDefeat);
-            if(player.isKnockedOut())
-            {
+            if (player.isKnockedOut()) {
                 printBattleResult(isWinner);
             }
         }
     }
-
-
-
 }
+    Card::Card(CardType type, const CardStats& stats): m_effect(type), m_stats(stats){}
 
-Card::Card(CardType type, const CardStats& stats): m_effect(type), m_stats(stats){}
 
-
-void Card::printInfo() const
-{
-    if (m_effect==CardType::Treasure)
+    void Card::printInfo() const
     {
-        printTreasureCardInfo(m_stats);
+        if (m_effect==CardType::Treasure)
+        {
+            printTreasureCardInfo(m_stats);
+        }
+        if (m_effect==CardType::Buff)
+        {
+            printBuffCardInfo(m_stats);
+        }
+        if(m_effect==CardType::Heal)
+        {
+            printHealCardInfo(m_stats);
+        }
+        if(m_effect==CardType::Battle)
+        {
+            printBattleCardInfo(m_stats);
+        }
     }
-    if (m_effect==CardType::Buff)
-    {
-        printBuffCardInfo(m_stats);
-    }
-     if(m_effect==CardType::Heal)
-    {
-        printHealCardInfo(m_stats);
-    }
-    if(m_effect==CardType::Battle)
-    {
-        printBattleCardInfo(m_stats);
-    }
-}
 
