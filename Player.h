899ac382fd4bec2dcefbf33a027b280024871bc7
larakh1 <103194777+larakh1@users.noreply.github.com>
2022-05-10 20:26:36 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -11,43 +11,72 @@ class Player{
 
 public:
     /**
-     *
-     * @param name
-     * @param maxHP
-     * @param force
+     * C'tor of Player class
+     * @param name - player's name
+     * @param maxHP- maximum health points
+     * @param force- player's force
      */
 
     Player(const char* name, int maxHP=MAX_DEFAULT_HP,int force=MAX_DEFAULT_FORCE);
-/**
- *
- * @param name
- * @param maxHP
- * @param force
- */
+
+
+    /*
+    * Here we are explicitly telling the compiler to use the default methods
+   */
+
     Player(const Player& player)=default;
 
     ~Player()=default;
 
     Player& operator=(const Player& player)=default;
-
+/**
+ * printing player's info
+ */
     void printInfo() const;
-
+/**
+ * leveling up the player until level 10
+ */
     void levelUp();
-
+/**
+ *
+ * @return the player's level
+ */
     int getLevel() const;
-
+/**
+ *
+ * @param add the amount of force we add to the player's force
+ */
     void buff(const int add);
-
+/**
+ *
+ * @param add the amount of hp we add to the player's hp, until it gets to the max_hp
+ */
     void heal(const int add);
-
+/**
+ *
+ * @param decrease the amount of hp we decrease from the player's hp
+ */
     void damage(const int decrease);
-
+/**
+ *
+ * @return true if the player is dead(the hp is 0),else it returns false
+ */
     bool isKnockedOut() const;
-
+ /**
+  *
+  * @param add the amount of coins that we add to the player coins
+  */
     void addCoins(const int add);
-
+/**
+ *
+ * @param price - the price that the player should pay
+ * @return true if the purchase has been done successfully,else it returns false
+ */
     bool pay(const int price);
-
+/**
+ *
+ * @return player's attack strength
+ */
     int getAttackStrength() const;
 
 
@@ -62,9 +91,6 @@ private:
     static const int MAX_DEFAULT_HP=100;
     static const int MAX_DEFAULT_FORCE=5;
 
-
 };
-
-
 
 #endif //EX2_Player_H
